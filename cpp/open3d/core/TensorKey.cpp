776d3e2e6892d15bd2d1d74e08f2945100c2a845
Yixing Lao <yixing.lao@gmail.com>
2020-12-23 23:36:44 -0800

    pimpl tensorkey

--- cpp/open3d/core/TensorKey.cpp
+++ cpp/open3d/core/TensorKey.cpp
@@ -26,6 +26,7 @@
 
 #include "open3d/core/TensorKey.h"
 
+#include <memory>
 #include <sstream>
 
 #include "open3d/core/Tensor.h"
@@ -34,154 +35,304 @@
 namespace open3d {
 namespace core {
 
-std::string TensorKey::ToString() const { return "bad"; }
+class TensorKey::Impl {
+public:
+    virtual ~Impl() {}
+};
 
-TensorKey TensorKey::Index(int64_t index) { return TensorKeyIndex(index); }
+class TensorKey::IndexImpl : public TensorKey::Impl {
+public:
+    IndexImpl(int64_t index) : index_(index) {}
+    int64_t GetIndex() const { return index_; }
+
+private:
+    int64_t index_;
+};
+
+class TensorKey::SliceImpl : public TensorKey::Impl {
+public:
+    SliceImpl(utility::optional<int64_t> start,
+              utility::optional<int64_t> stop,
+              utility::optional<int64_t> step)
+        : start_(start), stop_(stop), step_(step) {}
+    std::shared_ptr<SliceImpl> UpdateWithDimSize(int64_t dim_size) const {
+        return std::make_shared<SliceImpl>(
+                start_.has_value() ? start_.value() : 0,
+                stop_.has_value() ? stop_.value() : dim_size,
+                step_.has_value() ? step_.value() : 1);
+    }
+    int64_t GetStart() const {
+        if (start_.has_value()) {
+            return start_.value();
+        } else {
+            utility::LogError("TensorKeyMode::Slice: start is None.");
+        }
+    }
+    int64_t GetStop() const {
+        if (stop_.has_value()) {
+            return stop_.value();
+        } else {
+            utility::LogError("TensorKeyMode::Slice: stop is None.");
+        }
+    }
+    int64_t GetStep() const {
+        if (step_.has_value()) {
+            return step_.value();
+        } else {
+            utility::LogError("TensorKeyMode::Slice: step is None.");
+        }
+    }
+
+private:
+    utility::optional<int64_t> start_ = utility::nullopt;
+    utility::optional<int64_t> stop_ = utility::nullopt;
+    utility::optional<int64_t> step_ = utility::nullopt;
+};
+
+class TensorKey::IndexTensorImpl : public TensorKey::Impl {
+public:
+    IndexTensorImpl(const Tensor& index_tensor)
+        : index_tensor_(std::make_shared<Tensor>(index_tensor)) {}
+    std::shared_ptr<Tensor> GetIndexTensor() const { return index_tensor_; }
+
+private:
+    std::shared_ptr<Tensor> index_tensor_;
+};
+
+TensorKey::TensorKey(const std::shared_ptr<Impl>& impl) : impl_(impl) {}
+
+TensorKey::TensorKeyMode TensorKey::GetMode() const {
+    if (std::dynamic_pointer_cast<IndexImpl>(impl_)) {
+        return TensorKeyMode::Index;
+    } else if (std::dynamic_pointer_cast<SliceImpl>(impl_)) {
+        return TensorKeyMode::Slice;
+    } else if (std::dynamic_pointer_cast<IndexTensorImpl>(impl_)) {
+        return TensorKeyMode::IndexTensor;
+    } else {
+        utility::LogError("GetMode() failed: unsupported mode.");
+    }
+}
+
+std::string TensorKey::ToString() const { return "TODO"; }
+
+TensorKey TensorKey::Index(int64_t index) {
+    return TensorKey(std::make_shared<IndexImpl>(index));
+}
 
 TensorKey TensorKey::Slice(utility::optional<int64_t> start,
                            utility::optional<int64_t> stop,
                            utility::optional<int64_t> step) {
-    return TensorKeySlice(start, stop, step);
+    return TensorKey(std::make_shared<SliceImpl>(start, stop, step));
 }
 
 TensorKey TensorKey::IndexTensor(const Tensor& index_tensor) {
-    return TensorKeyIndexTensor(index_tensor);
+    return TensorKey(std::make_shared<IndexTensorImpl>(index_tensor));
 }
 
-TensorKey::TensorKeyMode TensorKey::GetMode() const { return mode_; }
-
+/// For TensorKeyMode::Index.
 int64_t TensorKey::GetIndex() const {
-    if (const TensorKeyIndex* tk = dynamic_cast<const TensorKeyIndex*>(this)) {
-        return tk->GetIndex();
+    if (auto index_impl = std::dynamic_pointer_cast<IndexImpl>(impl_)) {
+        return index_impl->GetIndex();
     } else {
-        utility::LogError(
-                "TensorKey::GetIndex() is invalid since the TensorKey is not a "
-                "TensorKeyIndex instance.");
+        utility::LogError("GetIndex() failed: the impl is not IndexImpl.");
     }
 }
 
+/// For TensorKeyMode::Slice.
 int64_t TensorKey::GetStart() const {
-    if (const TensorKeySlice* tk = dynamic_cast<const TensorKeySlice*>(this)) {
-        return tk->GetStart();
+    if (auto slice_impl = std::dynamic_pointer_cast<SliceImpl>(impl_)) {
+        return slice_impl->GetStart();
     } else {
-        utility::LogError(
-                "TensorKey::GetStart() is invalid since the TensorKey is not a "
-                "TensorKeySlice instance.");
+        utility::LogError("GetStart() failed: the impl is not SliceImpl.");
     }
 }
-
 int64_t TensorKey::GetStop() const {
-    if (const TensorKeySlice* tk = dynamic_cast<const TensorKeySlice*>(this)) {
-        return tk->GetStop();
+    if (auto slice_impl = std::dynamic_pointer_cast<SliceImpl>(impl_)) {
+        return slice_impl->GetStop();
     } else {
-        utility::LogError(
-                "TensorKey::GetStop() is invalid since the TensorKey is not a "
-                "TensorKeySlice instance.");
+        utility::LogError("GetStop() failed: the impl is not SliceImpl.");
     }
 }
-
 int64_t TensorKey::GetStep() const {
-    if (const TensorKeySlice* tk = dynamic_cast<const TensorKeySlice*>(this)) {
-        return tk->GetStep();
+    if (auto slice_impl = std::dynamic_pointer_cast<SliceImpl>(impl_)) {
+        return slice_impl->GetStep();
     } else {
-        utility::LogError(
-                "TensorKey::GetStep() is invalid since the TensorKey is not a "
-                "TensorKeySlice instance.");
+        utility::LogError("GetStep() failed: the impl is not SliceImpl.");
     }
 }
-
 TensorKey TensorKey::UpdateWithDimSize(int64_t dim_size) const {
-    if (const TensorKeySlice* tk = dynamic_cast<const TensorKeySlice*>(this)) {
-        return tk->UpdateWithDimSize(dim_size);
+    if (auto slice_impl = std::dynamic_pointer_cast<SliceImpl>(impl_)) {
+        return TensorKey(slice_impl->UpdateWithDimSize(dim_size));
     } else {
         utility::LogError(
-                "TensorKey::UpdateWithDimSize() is invalid since the TensorKey "
-                "is not a TensorKeySlice instance.");
+                "UpdateWithDimSize() failed: the impl is not SliceImpl.");
     }
 }
 
+/// For TensorKeyMode::IndexTensor.
 std::shared_ptr<Tensor> TensorKey::GetIndexTensor() const {
-    if (const TensorKeyIndexTensor* tk =
-                dynamic_cast<const TensorKeyIndexTensor*>(this)) {
-        return tk->GetIndexTensor();
+    if (auto index_tensor_impl =
+                std::dynamic_pointer_cast<IndexTensorImpl>(impl_)) {
+        return index_tensor_impl->GetIndexTensor();
     } else {
         utility::LogError(
-                "TensorKey::GetIndexTensor() is invalid since the TensorKey is "
-                "not a TensorKeyIndexTensor instance.");
+                "GetIndexTensor() failed: the impl is not IndexTensorImpl.");
     }
 }
 
-int64_t TensorKeyIndex::GetIndex() const { return index_; }
+// std::string TensorKey::ToString() const { return "bad"; }
 
-std::string TensorKeyIndex::ToString() const {
-    std::stringstream ss;
-    ss << "TensorKey::Index(" << index_ << ")";
-    return ss.str();
-}
+// TensorKey TensorKey::Index(int64_t index) { return TensorKeyIndex(index); }
 
-int64_t TensorKeySlice::GetStart() const {
-    if (start_.has_value()) {
-        return start_.value();
-    } else {
-        utility::LogError("TensorKeyMode::Slice: start is None.");
-    }
-}
+// TensorKey TensorKey::Slice(utility::optional<int64_t> start,
+//                            utility::optional<int64_t> stop,
+//                            utility::optional<int64_t> step) {
+//     return TensorKeySlice(start, stop, step);
+// }
 
-int64_t TensorKeySlice::GetStop() const {
-    if (stop_.has_value()) {
-        return stop_.value();
-    } else {
-        utility::LogError("TensorKeyMode::Slice: stop is None.");
-    }
-}
+// TensorKey TensorKey::IndexTensor(const Tensor& index_tensor) {
+//     return TensorKeyIndexTensor(index_tensor);
+// }
 
-int64_t TensorKeySlice::GetStep() const {
-    if (step_.has_value()) {
-        return step_.value();
-    } else {
-        utility::LogError("TensorKeyMode::Slice: step is None.");
-    }
-}
+// TensorKey::TensorKeyMode TensorKey::GetMode() const { return mode_; }
 
-TensorKey TensorKeySlice::UpdateWithDimSize(int64_t dim_size) const {
-    return TensorKeySlice(start_.has_value() ? start_.value() : 0,
-                          stop_.has_value() ? stop_.value() : dim_size,
-                          step_.has_value() ? step_.value() : 1);
-}
+// int64_t TensorKey::GetIndex() const {
+//     if (const TensorKeyIndex* tk = dynamic_cast<const TensorKeyIndex*>(this))
+//     {
+//         return tk->GetIndex();
+//     } else {
+//         utility::LogError(
+//                 "TensorKey::GetIndex() is invalid since the TensorKey is not
+//                 a " "TensorKeyIndex instance.");
+//     }
+// }
 
-std::string TensorKeySlice::ToString() const {
-    std::stringstream ss;
-    ss << "TensorKey::Slice(";
-    if (start_.has_value()) {
-        ss << start_.value();
-    } else {
-        ss << "None";
-    }
-    ss << ", ";
-    if (stop_.has_value()) {
-        ss << stop_.value();
-    } else {
-        ss << "None";
-    }
-    ss << ", ";
-    if (step_.has_value()) {
-        ss << step_.value();
-    } else {
-        ss << "None";
-    }
-    ss << ")";
-    return ss.str();
-}
+// int64_t TensorKey::GetStart() const {
+//     if (const TensorKeySlice* tk = dynamic_cast<const TensorKeySlice*>(this))
+//     {
+//         return tk->GetStart();
+//     } else {
+//         utility::LogError(
+//                 "TensorKey::GetStart() is invalid since the TensorKey is not
+//                 a " "TensorKeySlice instance.");
+//     }
+// }
 
-std::shared_ptr<Tensor> TensorKeyIndexTensor::GetIndexTensor() const {
-    return index_tensor_;
-}
+// int64_t TensorKey::GetStop() const {
+//     if (const TensorKeySlice* tk = dynamic_cast<const TensorKeySlice*>(this))
+//     {
+//         return tk->GetStop();
+//     } else {
+//         utility::LogError(
+//                 "TensorKey::GetStop() is invalid since the TensorKey is not a
+//                 " "TensorKeySlice instance.");
+//     }
+// }
 
-std::string TensorKeyIndexTensor::ToString() const {
-    std::stringstream ss;
-    ss << "TensorKey::IndexTensor(" << index_tensor_->ToString() << ")";
-    return ss.str();
-}
+// int64_t TensorKey::GetStep() const {
+//     if (const TensorKeySlice* tk = dynamic_cast<const TensorKeySlice*>(this))
+//     {
+//         return tk->GetStep();
+//     } else {
+//         utility::LogError(
+//                 "TensorKey::GetStep() is invalid since the TensorKey is not a
+//                 " "TensorKeySlice instance.");
+//     }
+// }
+
+// TensorKey TensorKey::UpdateWithDimSize(int64_t dim_size) const {
+//     if (const TensorKeySlice* tk = dynamic_cast<const TensorKeySlice*>(this))
+//     {
+//         return tk->UpdateWithDimSize(dim_size);
+//     } else {
+//         utility::LogError(
+//                 "TensorKey::UpdateWithDimSize() is invalid since the
+//                 TensorKey " "is not a TensorKeySlice instance.");
+//     }
+// }
+
+// std::shared_ptr<Tensor> TensorKey::GetIndexTensor() const {
+//     if (const TensorKeyIndexTensor* tk =
+//                 dynamic_cast<const TensorKeyIndexTensor*>(this)) {
+//         return tk->GetIndexTensor();
+//     } else {
+//         utility::LogError(
+//                 "TensorKey::GetIndexTensor() is invalid since the TensorKey
+//                 is " "not a TensorKeyIndexTensor instance.");
+//     }
+// }
+
+// int64_t TensorKeyIndex::GetIndex() const { return index_; }
+
+// std::string TensorKeyIndex::ToString() const {
+//     std::stringstream ss;
+//     ss << "TensorKey::Index(" << index_ << ")";
+//     return ss.str();
+// }
+
+// int64_t TensorKeySlice::GetStart() const {
+//     if (start_.has_value()) {
+//         return start_.value();
+//     } else {
+//         utility::LogError("TensorKeyMode::Slice: start is None.");
+//     }
+// }
+
+// int64_t TensorKeySlice::GetStop() const {
+//     if (stop_.has_value()) {
+//         return stop_.value();
+//     } else {
+//         utility::LogError("TensorKeyMode::Slice: stop is None.");
+//     }
+// }
+
+// int64_t TensorKeySlice::GetStep() const {
+//     if (step_.has_value()) {
+//         return step_.value();
+//     } else {
+//         utility::LogError("TensorKeyMode::Slice: step is None.");
+//     }
+// }
+
+// TensorKey TensorKeySlice::UpdateWithDimSize(int64_t dim_size) const {
+//     return TensorKeySlice(start_.has_value() ? start_.value() : 0,
+//                           stop_.has_value() ? stop_.value() : dim_size,
+//                           step_.has_value() ? step_.value() : 1);
+// }
+
+// std::string TensorKeySlice::ToString() const {
+//     std::stringstream ss;
+//     ss << "TensorKey::Slice(";
+//     if (start_.has_value()) {
+//         ss << start_.value();
+//     } else {
+//         ss << "None";
+//     }
+//     ss << ", ";
+//     if (stop_.has_value()) {
+//         ss << stop_.value();
+//     } else {
+//         ss << "None";
+//     }
+//     ss << ", ";
+//     if (step_.has_value()) {
+//         ss << step_.value();
+//     } else {
+//         ss << "None";
+//     }
+//     ss << ")";
+//     return ss.str();
+// }
+
+// std::shared_ptr<Tensor> TensorKeyIndexTensor::GetIndexTensor() const {
+//     return index_tensor_;
+// }
+
+// std::string TensorKeyIndexTensor::ToString() const {
+//     std::stringstream ss;
+//     ss << "TensorKey::IndexTensor(" << index_tensor_->ToString() << ")";
+//     return ss.str();
+// }
 
 }  // namespace core
 }  // namespace open3d
--- cpp/open3d/core/TensorKey.h
+++ cpp/open3d/core/TensorKey.h
@@ -58,85 +58,91 @@ class TensorKey {
 public:
     enum class TensorKeyMode { Index, Slice, IndexTensor };
     TensorKeyMode GetMode() const;
+    std::string ToString() const;
+
+    ~TensorKey() {}
+
     static TensorKey Index(int64_t index);
     static TensorKey Slice(utility::optional<int64_t> start,
                            utility::optional<int64_t> stop,
                            utility::optional<int64_t> step);
     static TensorKey IndexTensor(const Tensor& index_tensor);
 
+public:
+    /// For TensorKeyMode::Index.
     int64_t GetIndex() const;
 
-    int64_t GetStart() const;
-
-    int64_t GetStop() const;
-
-    int64_t GetStep() const;
-
-    std::shared_ptr<Tensor> GetIndexTensor() const;
-
-    TensorKey UpdateWithDimSize(int64_t dim_size) const;
-
-    virtual std::string ToString() const;
-    virtual ~TensorKey() {}
-
-protected:
-    TensorKey(TensorKeyMode mode) : mode_(mode) {}
-    TensorKeyMode mode_;
-};
-
-class TensorKeyIndex : public TensorKey {
-public:
-    TensorKeyIndex(int64_t index)
-        : TensorKey(TensorKeyMode::Index), index_(index) {}
-    int64_t GetIndex() const;
-    std::string ToString() const override;
-
-protected:
-    int64_t index_ = 0;
-};
-
-class TensorKeySlice : public TensorKey {
-public:
-    TensorKeySlice(utility::optional<int64_t> start,
-                   utility::optional<int64_t> stop,
-                   utility::optional<int64_t> step)
-        : TensorKey(TensorKeyMode::Slice),
-          start_(start),
-          stop_(stop),
-          step_(step) {}
-    /// When dim_size is know, convert the slice object such that
-    /// start_is_none_ == stop_is_none_ == step_is_none_ == false
-    /// E.g. if t.shape == (5,), t[:4]:
-    ///      before compute: Slice(None,    4, None)
-    ///      after compute : Slice(   0,    4,    1)
-    /// E.g. if t.shape == (5,), t[1:]:
-    ///      before compute: Slice(   1, None, None)
-    ///      after compute : Slice(   1,    5,    1)
-    TensorKey UpdateWithDimSize(int64_t dim_size) const;
+    /// For TensorKeyMode::Slice.
     int64_t GetStart() const;
     int64_t GetStop() const;
     int64_t GetStep() const;
-    std::string ToString() const override;
+    TensorKey UpdateWithDimSize(int64_t dim_size) const;
 
-protected:
-    utility::optional<int64_t> start_ = utility::nullopt;
-    utility::optional<int64_t> stop_ = utility::nullopt;
-    utility::optional<int64_t> step_ = utility::nullopt;
-};
-
-class TensorKeyIndexTensor : public TensorKey {
-public:
-    TensorKeyIndexTensor(const Tensor& index_tensor)
-        : TensorKey(TensorKeyMode::IndexTensor),
-          index_tensor_(std::make_shared<Tensor>(index_tensor)) {}
+    /// For TensorKeyMode::IndexTensor.
     std::shared_ptr<Tensor> GetIndexTensor() const;
-    std::string ToString() const override;
 
-protected:
-    /// To avoid circular include, the pointer type is used. The index_tensor is
-    /// shallow-copied when the TensorKey constructor is called.
-    std::shared_ptr<Tensor> index_tensor_;
+private:
+    class Impl;
+    class IndexImpl;
+    class SliceImpl;
+    class IndexTensorImpl;
+    std::shared_ptr<Impl> impl_;
+    TensorKey(const std::shared_ptr<Impl>& impl);
 };
+
+// class TensorKeyIndex : public TensorKey {
+// public:
+//     TensorKeyIndex(int64_t index)
+//         : TensorKey(TensorKeyMode::Index), index_(index) {}
+//     int64_t GetIndex() const;
+//     std::string ToString() const override;
+
+// protected:
+//     int64_t index_ = 0;
+// };
+
+// class TensorKeySlice : public TensorKey {
+// public:
+//     TensorKeySlice(utility::optional<int64_t> start,
+//                    utility::optional<int64_t> stop,
+//                    utility::optional<int64_t> step)
+//         : TensorKey(TensorKeyMode::Slice),
+//           start_(start),
+//           stop_(stop),
+//           step_(step) {}
+//     /// When dim_size is know, convert the slice object such that
+//     /// start_is_none_ == stop_is_none_ == step_is_none_ == false
+//     /// E.g. if t.shape == (5,), t[:4]:
+//     ///      before compute: Slice(None,    4, None)
+//     ///      after compute : Slice(   0,    4,    1)
+//     /// E.g. if t.shape == (5,), t[1:]:
+//     ///      before compute: Slice(   1, None, None)
+//     ///      after compute : Slice(   1,    5,    1)
+//     TensorKey UpdateWithDimSize(int64_t dim_size) const;
+//     int64_t GetStart() const;
+//     int64_t GetStop() const;
+//     int64_t GetStep() const;
+//     std::string ToString() const override;
+
+// protected:
+//     utility::optional<int64_t> start_ = utility::nullopt;
+//     utility::optional<int64_t> stop_ = utility::nullopt;
+//     utility::optional<int64_t> step_ = utility::nullopt;
+// };
+
+// class TensorKeyIndexTensor : public TensorKey {
+// public:
+//     TensorKeyIndexTensor(const Tensor& index_tensor)
+//         : TensorKey(TensorKeyMode::IndexTensor),
+//           index_tensor_(std::make_shared<Tensor>(index_tensor)) {}
+//     std::shared_ptr<Tensor> GetIndexTensor() const;
+//     std::string ToString() const override;
+
+// protected:
+//     /// To avoid circular include, the pointer type is used. The index_tensor
+//     /// is shallow-copied when the TensorKey constructor is called.
+//     std::shared_ptr<Tensor> index_tensor_;
+// };
 
 }  // namespace core
 }  // namespace open3d
